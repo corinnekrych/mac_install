cfab1837672a1c8a7ca10a426e845245d45b4c71
Corinne Krych <corinnekrych@gmail.com>
2017-04-03 16:46:49 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Laptop setup
+
+Welcome to the mac_insatll script!
+The awsome script that turns your mac into a killer development machine.
+
+My dev profile is: 
+- mobile dev (Xcode, CocoaPods, Cordova)
+- JavaScript dev (nvm, node)
+
+## Customize script
+
+### Set-up for GPG and SSH key
+
+In the same folder where you got you script, your should have:
+* `pub.gpg` and `sec.gpg` files
+* ssh.tar which contain your .ssh content
+
+In `mac_install.sh` script customize GPG by adding your GPG key.
+
+To generate those files, follow the step below:
+
+#### Export you GPG
+If you have a GPG key you want to import in your new macOS configuration, export you public and private key:
+
+```
+gpg --output pub.gpg --armor --export YOUR_KEY
+gpg --output sec.gpg --armor --export-secret-key YOUR_KEY
+```
+
+#### Tar your .ssh 
+To keep the correct rights:
+```
+tar cvpf ssh.tar ./ssh
+```
+## Github repos
+
+If you want to clone your working repos in `$HOME/workspace` folder:
+* add a file `github.txt` which contains a git repo per line. Similar to [feedhenry_github.txt](feedhenry_github.txt)
+* in  `./mac_install.sh` call the function to clone them all with the `prefix` of `prefix_github.com`:
+```
+clone_repo "feedhenry"
+```
+
+## Run the script
+```
+. ./mac_install.sh
+```
+use “dot space dot slash”, to execute the script in the current shell without forking a sub shell.
---
+++ mac_install.sh
@@ -0,0 +1,250 @@
+#!/bin/bash
+
+# Welcome to the mac_insatll script!
+# The awsome script that turns your mac into a killer 
+# development machine.
+echo "==> Welcome to mac_install."
+# Homebrew install
+if test ! $(which brew)
+then
+	echo "==> Install Homebrew"
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Install cask for brew
+echo "==> Install Cask"
+brew tap caskroom/cask
+
+# Update brew
+echo "==> Update Homebrew"
+brew update
+brew upgrade brew-cask
+
+# Install mas (Mac App Store)
+echo "==> Install mas for Mac App Store."
+brew install mas
+
+
+# Function to install from App Store with mas (source : https://github.com/argon/mas/issues/41#issuecomment-245846651)
+function install () {
+	# Check if the App is already installed
+	mas list | grep -i "$1" > /dev/null
+
+	if [ "$?" == 0 ]; then
+		echo "==>==> $1 already installed!"
+	else
+		echo "==>==> Installing $1..."
+		mas search "$1" | { read app_ident app_name ; mas install $app_ident ; }
+	fi
+}
+
+function app_is_installed() {
+  local app_name
+  app_name=$(echo "$1" | cut -d'-' -f1)
+  find /Applications -iname "$app_name*" -maxdepth 1 | egrep '.*' > /dev/null
+}
+
+function app_is_installed_cask() {
+  local app_name
+  app_name=$(echo "$1" | cut -d'-' -f1)
+  find /usr/local/Caskroom -iname "$app_name*" -maxdepth 1 | egrep '.*' > /dev/null
+}
+
+function app_is_installed_homebrew() {
+  local app_name
+  app_name=$(echo "$1" | cut -d'-' -f1)
+  find /usr/local/Cellar -iname "$app_name*" -maxdepth 1 | egrep '.*' > /dev/null
+}
+
+function append_to_file() {
+  local file="$1"
+  local text="$2"
+
+  if ! grep -qs "^$text$" "$file"; then
+    printf "\n%s\n" "$text" >> "$file"
+  fi
+}
+
+# Install with brew cask
+function cask_install() {
+	if ! app_is_installed_cask $1; then
+		if ! app_is_installed $1; then
+			echo "==> Install $1"
+			brew cask install $1
+			if [[ $2 ]]; then 
+				echo "==>==> Dock $2"
+				defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/$2.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+			fi
+		fi
+	fi
+}
+# Install with brew
+function brew_install() {
+	if ! app_is_installed_homebrew $1; then
+		if ! app_is_installed $1; then
+			echo "==> Install $1"
+			brew cask install $1
+			if [[ $2 ]]; then 
+				echo "==>==> Dock $2"
+				defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/$2.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+			fi
+		fi
+	fi
+}
+echo "after function definition"
+cask_install java
+brew_install wget 
+brew_install ack 
+brew_install maven
+brew_install gpg 
+brew_install zsh zsh-completions 
+brew_install git
+cask_install iterm2 
+cask_install sublime-text 
+cask_install atom atom
+cask_install firefox firefox
+cask_install dropbox 
+cask_install colloquy colloquy
+cask_install google-chrome "Google Chrome"
+cask_install diffmerge
+cask_install mattermost mattermost
+cask_install intellij-idea "intelliJ IDEA"
+cask_install webstorm webstorm
+cask_install slack slack
+if ! app_is_installed_cask 'blue-jeans-launcher'; then
+	echo "==> Setup for BlueJeans app"
+	brew cask install blue-jeans-launcher
+	open /usr/local/Caskroom/blue-jeans-launcher/1.6.8/Blue\ Jeans\ Launcher.app
+fi
+
+# Install oh-my-zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+	echo "==> Install oh-my-zsh"
+	chsh -s $(which zsh)
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+# Install rvm
+if [ ! -d "$HOME/.rvm" ]; then
+	echo "==> Install rvm"
+	curl -L https://get.rvm.io | bash -s stable
+	. ~/.bash_profile
+	rvm install 2.3.0 --disable-binary
+# TODO rvm --default use 2.3.0
+# TODO to import GPG curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+# Install JavaScript environment
+fi
+if [ ! -d "$HOME/.nvm" ]; then
+	echo "==> Install nvm"
+	curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+	append_to_file "$HOME/.zshrc" 'export NVM_DIR="$HOME/.nvm"'
+	append_to_file "$HOME/.zshrc" '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm'
+	source "$HOME/.zshrc"
+	nvm install v7.8.0
+	nvm install v4.4.4
+	nvm install v6.5.0
+	nvm alias default v6.5.0
+fi
+
+# Set-up GPG, SSH Keys
+if [ -f "./gpg_pub.gpg" ]; then
+	echo "==> Set-up GPG key"
+	gpg --import ./gpg_pub.gpg
+	gpg --allow-secret-key-import --import ./gpg_sec.gpg 
+	# TODO replace FD8D377A with your GPG key
+	echo "$( \
+	   gpg --list-keys --fingerprint \
+	   | grep FD8D377A -A 1 | tail -1 \
+	   | tr -d '[:space:]' | awk 'BEGIN { FS = "=" } ; { print $2 }' \
+	 ):6:" | gpg --import-ownertrust;
+fi
+
+# Set-up SSH
+if [ -f "./ssh.tar" ]; then
+	echo "==> Set-up .ssh"
+	tar xvpf ./ssh.tar --directory ~
+	echo "==> Set-up git"
+ 	git config --global user.name "Corinne Krych"
+ 	git config --global user.email corinnekrych@gmail.com
+fi
+
+# Set-up chat, irc
+if [ -f "./colloquy.backup" ]; then
+	echo "==> Import IRC chat settings"
+	defaults import info.colloquy colloquy.backup
+fi
+
+# Set-up workspace with relevant git repos
+function clone_repo () {
+    
+    repos=() # Create array
+    while IFS= read -r line # Read a line
+    do
+        repos+=("$line") # Append line to the array
+    done < "$1_github.txt"
+
+	current_dir=$(pwd)
+	echo "==> Create workspace"
+	mkdir -p ~/workspace
+	mkdir -p ~/workspace/$1
+	cd ~/workspace/$1
+	for repo in ${repos[@]}
+	do
+		last_string=$(echo $repo| cut -d'/' -f 2)
+		repo_directory=$(echo $last_string| cut -d'.' -f 1)
+		if [ ! -d "$repo_directory" ]; then
+			echo "==> Clone $repo"
+			git clone $repo
+		else
+		echo "==> Directory $repo_directory already created."
+		fi
+	done
+	cd  $current_dir
+}
+echo "==> Clone feedhenry repos"
+clone_repo "feedhenry"
+echo "==> Clone devtools platform repos"
+clone_repo "devtools"
+
+# Install with mas
+if ! app_is_installed 'Xcode'; then
+	echo "= Enter your AppleID:" 
+	read APPLE_ID
+	echo "==> Enter your Apple ID’s password: $APPLE_ID"
+	read -s PASSWORD
+	mas signin $APPLE_ID "$PASSWORD"
+	echo "==> Install Xcode"
+	install "Xcode"
+fi
+
+# Install with gem
+if [ ! -d "$HOME/.cocoapods" ]; then
+	echo "==> Install CocoaPods"
+	sudo gem install cocoapods
+fi
+
+# Finder 
+echo "==> Set-up Finder"
+chflags nohidden ~/Library
+defaults write com.apple.finder AppleShowAllFiles TRUE
+defaults write com.apple.finder ShowStatusBar -bool true
+defaults write com.apple.finder FXPreferredViewStyle -string “Nlsv”
+defaults write com.apple.finder ShowPathbar -bool true
+sudo defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+# TODO settings -> keyboard -> shortcuts -> services -> files and folders New Terminal at folder / New terminal tab at folder => on
+
+# Print home folder
+defaults write com.apple.finder NewWindowTarget -string "PfHm"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
+
+# Search in current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Do not create .DS_STORE
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Cleaning
+killall Dock
+killall Finder
+brew cleanup
+rm -rf /Library/Caches/Homebrew/
